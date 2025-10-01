f1503eb85a96a42173b68b83e7f7a21cea7cac0f
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-01 10:17:16 +0200

    fix(deploy): add basePath configuration on next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/pagopa-ecommerce-watchdog-deadletter-fe',
 };
 
 export default nextConfig;
