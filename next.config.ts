0d69a7666446185e4ac2093586ab1df41d6edf3e
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-01 17:46:43 +0200

    fix(deploy): remove basePath in next.config and values-dev

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  basePath: '/pagopa-ecommerce-watchdog-deadletter-fe',
 };
 
 export default nextConfig;
