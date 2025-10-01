2557f3024ef50595dbd2ed8db0e6bbf7273016d8
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-01 17:57:31 +0200

    fix(deploy): add assetPrefix in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  assetPrefix: 'pagopa-ecommerce-watchdog-deadletter-fe',
 };
 
 export default nextConfig;
