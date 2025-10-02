073442d4af61f8d8b1c60b6180b4ad9a8b0dc27b
Ivan Ferrando <ivan.ferrando@pagopa.it>
2025-10-02 09:37:22 +0200

    feat(pipelines): PIDM-999 add deploy pipeline (#16)
    * feat(pipelines): add deploy pipeline
    
    * feat(deploy): add helm-microservice-chart-deploy template
    
    * feat(pipelines): fix deploy pipeline
    
    * feat(pipelines): add chart-current-version template
    
    * fix(deploy): add helm envConfig empty values
    
    * fix(deploy): add liveness + readiness probes and empty envSecret to values-dev
    
    * fix(deploy): set secretProviderClass create to false on values-dev
    
    * fix(deploy): update liveness and readiness path
    
    * fix(values-dev): add /var/cache/nginx temp volume mount
    
    * fix(values-dev): set readOnlyRootFilesystem to false
    
    * fix(values-dev): set allowPrivilegeEscalation to true
    
    * fix(deploy): update dockerfile
    
    * fix(deploy) update port to 8080
    
    * feat(dockerfile): change web server to serve
    
    * fix(values-dev): update port to 3000
    
    * fix(deploy): add basePath configuration on next.config
    
    * feat(values-dev) add NEXT_PUBLIC_ECOMMERCE_WATCHDOG_SERVICE_API_HOST value
    
    * feat(deploy): add dev env file
    
    * feat(deploy): add dev env vars to deploy pipeline
    
    * fix(deploy): fix DOCKER_BUILD_ARGS value
    
    * fix(deploy): update dockerfile
    
    * fix(values-dev): update readiness and liveness paths
    
    * fix(values-dev): update readiness and liveness paths
    
    * fix(deploy): remove basePath in next.config
    
    * fix(deploy): update dockerfile
    
    * fix(values-dev): update readiness and liveness paths
    
    * fix(deploy): remove basePath in next.config and values-dev
    
    * fix(deploy): add assetPrefix in next.config
    
    * fix(deploy): fix assetPrefix
    
    * fix(deploy): fix backend API host + lower initialDelaySeconds to 5 seconds

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: 'standalone',
+  assetPrefix: '/pagopa-ecommerce-watchdog-deadletter-fe',
 };
 
 export default nextConfig;
