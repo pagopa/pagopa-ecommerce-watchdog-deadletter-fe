04e6e594f4e9e003063220f08ad583f3965c471e
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-17 09:38:06 +0200

    feat(auth): remove GET /users and x-user-id header

--- src/app/utils/api/client.ts
+++ src/app/utils/api/client.ts
@@ -1,7 +1,6 @@
 import { DeadletterAction } from "@/app/types/DeadletterAction";
 import { DeadletterResponse } from "@/app/types/DeadletterResponse";
 import { AuthenticationCredential, AuthenticationOk } from "@/app/types/Authentication";
-import { JwtUser } from "@pagopa/mui-italia";
 
 export const fetchAuthentication = async (user: AuthenticationCredential): Promise<AuthenticationOk | null> => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_ECOMMERCE_WATCHDOG_AUTH_API_HOST}/authenticate`, {
@@ -24,32 +23,12 @@ export const fetchAuthentication = async (user: AuthenticationCredential): Promi
 
 };
 
-export const fetchUserData = async (token: string): Promise<JwtUser | null> => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_ECOMMERCE_WATCHDOG_AUTH_API_HOST}/users`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    if (!res.ok) throw new Error("Failed to fetch user");
-    const data = await res.json();
-    return {
-      id: data.id,
-      name: data.name,
-      surname: data.surname,
-      email: data.email
-    };
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-};
-
 export const fetchActionsByTransactionId = async (token: string, transactionId: string): Promise<DeadletterAction[]> => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_ECOMMERCE_WATCHDOG_SERVICE_API_HOST}/deadletter-transactions/${transactionId}/actions`, {
-      headers: { Authorization: `Bearer ${token}`,
-                 "x-user-id": "test-user-id"}
+      headers: { 
+        Authorization: `Bearer ${token}`
+      }
     });
     if (!res.ok) throw new Error(`Failed to fetch actions for ${transactionId}`);
     return await res.json();
@@ -63,8 +42,7 @@ export const fetchDeadletterTransactions = async (token: string, date: string): 
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_ECOMMERCE_WATCHDOG_SERVICE_API_HOST}/deadletter-transactions?date=${date}&pageNumber=0&pageSize=10`, {
       headers: {
-        Authorization: `Bearer ${token}`,
-         "x-user-id": "test-user-id"
+        Authorization: `Bearer ${token}`
       }
     });
     if (!res.ok) throw new Error("Failed to fetch deadletter transactions");
