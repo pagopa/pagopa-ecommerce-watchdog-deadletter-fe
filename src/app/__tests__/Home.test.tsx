5a595b62ea79c99faffe48b62167ea471648073e
sterondinella <stefano.rondinella@pagopa.it>
2025-11-10 12:34:40 +0100

    feat(Home.test): add test for check the currect load of the data

--- src/app/__tests__/Home.test.tsx
+++ src/app/__tests__/Home.test.tsx
@@ -1,5 +1,5 @@
 import Home from "../page"
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, logRoles } from '@testing-library/react';
 import LoginDialog from "../LoginDialog";
 import { fetchAuthentication, fetchActions, fetchActionsByTransactionId, fetchAddActionToDeadletterTransaction, fetchDeadletterTransactions } from '../utils/api/client';
 import React from "react";
@@ -9,6 +9,7 @@ import { decodeJwt } from 'jose';
 import { AuthenticationOk } from '../types/Authentication';
 import { JwtUser } from "@pagopa/mui-italia";
 import { getTokenFromUrl } from "../utils/utils";
+import {deadletterResponse} from "./mock/DataMocks";
 
 
 // Mocks
@@ -18,6 +19,14 @@ jest.mock('../utils/api/client', () => ({
   fetchActionsByTransactionId: jest.fn(),
   fetchAddActionToDeadletterTransaction: jest.fn(),
   fetchDeadletterTransactions: jest.fn(),
+}));
+
+// Mock for ResizeObserver
+// ResizeObserver is used by DataGrid and Recharts, need a mock because does not exist in jsdom
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
 }));
 
 jest.mock('jose', () => ({
@@ -55,6 +64,8 @@ const mockSessionStorage = (() => {
 })();
 Object.defineProperty(window, 'sessionStorage', { value: mockSessionStorage, writable: true, });
 
+
+
 describe('Home', () => {
 
   beforeEach(() => {
@@ -72,7 +83,7 @@ describe('Home', () => {
   });
 
   const renderComponent = () => {
-    render(<Home />)
+    return render(<Home />)
   }
 
   it('check that when the Home is rendered the LoginDialog is open if the user is not logged', () => {
@@ -113,7 +124,7 @@ describe('Home', () => {
 
   it('check that, after the login, if we select a date it will show a table', () => {});
 
-  it('check that clicking the logout it log out from the application', async () => {
+  it('check that clicking the button with the text "Esci" it log out from the application', async () => {
     // Mock the presence of a token in the sessionStorage
     const tokenMock: string = "mockToken123";
     mockSessionStorage.setItem("authToken", tokenMock);
@@ -139,6 +150,84 @@ describe('Home', () => {
     // Check that the logout is done
     expect(sessionStorage.clear).toHaveBeenCalled();
   });
+
+  it('check that clicking the Login after logout open the dialog', async () => {
+    // Mock the presence of a token in the sessionStorage
+    const tokenMock: string = "mockToken123";
+    mockSessionStorage.setItem("authToken", tokenMock);
+
+    const mockUser: JwtUser = {
+      name: 'Mario',
+      surname: 'Rossi',
+      email: 'mario.rossi@example.com',
+      id: 'testId'
+    };
+    mockSessionStorage.setItem("jwtUser", JSON.stringify(mockUser));
+
+    mockedFetchActions.mockResolvedValue([]);
+
+    renderComponent();
+
+    const userButton = await screen.findByText("Mario Rossi");
+    await userEvent.click(userButton);
+    // Press the logout button
+    const logoutButton = await screen.findByText("Esci");
+    await userEvent.click(logoutButton);
+
+    // Check that the logout is done
+    expect(sessionStorage.clear).toHaveBeenCalled();
+
+    const loginButton = await screen.findByText("Accedi");
+    await userEvent.click(loginButton);
+    expect(screen.getByRole('dialog', { name: "Login" })).toBeInTheDocument();
+  });
+
+  it('check that selecting a date it will show the table and the graphs', async () => {
+    // Mock the presence of a token in the sessionStorage would be logged without login needed
+    const tokenMock: string = "mockToken123";
+    mockSessionStorage.setItem("authToken", tokenMock);
+    const mockUser: JwtUser = {
+      name: 'Mario',
+      surname: 'Rossi',
+      email: 'mario.rossi@example.com',
+      id: 'testId'
+    };
+    mockSessionStorage.setItem("jwtUser", JSON.stringify(mockUser));
+
+    // Mock the api
+    mockedFetchDeadletterTransactions.mockResolvedValue(deadletterResponse);
+    mockedFetchActionsByTransactionId.mockResolvedValue([]);
+    mockedFetchActions.mockResolvedValue([]);
+    
+    renderComponent();
+
+    // wait until the user is logged
+    expect(await screen.findByText("Mario Rossi")).toBeInTheDocument();
+
+    // check the presence of the date picker
+    const datePicker = await screen.findByLabelText("Data transazioni in deadletter");
+    expect(datePicker).toBeInTheDocument();
+
+    // click on the date picker and select a date
+    await userEvent.type(datePicker, "2025-11-07");
+
+    expect(datePicker).toHaveValue("2025-11-07");
+
+    //expect(await screen.findByTestId('mock-charts-statistics')).toBeInTheDocument();
+
+    // wait until the graphs and the table are in the document
+    expect(await screen.findByText("Stato Ecommerce")).toBeInTheDocument();
+    expect(await screen.findByText("Stato NPG")).toBeInTheDocument();
+    expect(await screen.findByText("Distribuzione metodi di pagamento")).toBeInTheDocument();
+    expect(await screen.findByText("Distribuzione stato azioni")).toBeInTheDocument();
+    expect(await screen.findByRole("grid")).toBeInTheDocument();  
+  
+
+    expect(mockedFetchDeadletterTransactions).toHaveBeenCalled();
+    expect(mockedFetchActionsByTransactionId).toHaveBeenCalled();
+
+  });
+
 
   /* it('', () => {}); */
 
