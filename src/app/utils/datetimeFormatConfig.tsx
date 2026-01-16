b67d66f6194c5eff2e6769c4dfccbeced86ba149
ivan.ferrando <ivan.ferrando@pagopa.it>
2026-01-16 09:34:20 +0100

    feat(date-format): add DateTime format configs

---
+++ src/app/utils/datetimeFormatConfig.tsx
@@ -0,0 +1,25 @@
+export const dateTimeLocale = 'it-IT';
+
+export const utcDateTimeFormatOptions: Intl.DateTimeFormatOptions = {
+    timeZone: 'UTC',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+
+export const dateTimeFormatOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+};
+
+export const extendedMonthDateFormatOptions: Intl.DateTimeFormatOptions = { 
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+};
