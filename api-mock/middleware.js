ba191cf1b1ef4a14954ab1abc5fbc20c2a63347f
ivan.ferrando <ivan.ferrando@pagopa.it>
2026-03-05 11:33:24 +0100

    feat(notes): update api mock to handle the POST-PUT-DELETE /notes

--- api-mock/middleware.js
+++ api-mock/middleware.js
@@ -1,5 +1,24 @@
-module.exports = function (req, res, next) {
-  const db = req.app.db
+module.exports = function middleware (req, res, next) {
+
+  // POST /deadletter-transactions/{transactionId}/notes
+  const match = req.path.match("(/deadletter-transactions/(.+)/notes)");
+  if (req.method === 'POST' && match) {
+    const now = new Date().toISOString();
+    const mockedResponse = {
+      ...req.body,
+      transactionId: match[2],
+      userId: "mario.rossi",
+      noteId: crypto.randomUUID(),
+      createdAt: now,
+      updatedAt: now
+    };
+    return res.status(201).json(mockedResponse);
+  }
+
+  if (req.method === 'PUT' || req.method === 'DELETE') {
+    return res.sendStatus(200);
+  }
+
   if (req.method === 'POST') {
     // Converts POST to GET
     req.method = 'GET'
@@ -7,12 +26,12 @@ module.exports = function (req, res, next) {
   const requestPath = req.path.toString();
   const requestBody = req.body;
   console.log(`${new Date().toISOString()} - Received request: ${req.method} ${req.path}
-     Headers:
-     ${JSON.stringify(req.headers)}
-     Path: ${requestPath}
-     Body:
-     ${JSON.stringify(req.body)}
-     `);
+    Headers:
+    ${JSON.stringify(req.headers)}
+    Path: ${requestPath}
+    Body:
+    ${JSON.stringify(requestBody)}
+    `);
 
   next();
 }
